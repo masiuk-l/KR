754c05e575cfe72ac0bb03757c263eb9c8ee8185
masiuk-l <masiuk-l@ya.ru>
2017-08-06 21:37:47 +0300

    add db  connection pool

--- src/db/ConnectionManager.java
+++ src/db/ConnectionManager.java
@@ -1,9 +1,10 @@
 package db;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.log4j.Logger;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
@@ -12,20 +13,38 @@ import java.util.ResourceBundle;
  */
 public class ConnectionManager {
     final static Logger LOG = Logger.getLogger(ConnectionManager.class);
+    private static ConnectionManager connectionManager;
+    private ComboPooledDataSource cpds;
 
-    public static Connection getConnection() throws SQLException {
+    private ConnectionManager() throws SQLException {
         ResourceBundle resource = ResourceBundle.getBundle("database");
         String url = resource.getString("url");
         String driver = resource.getString("driver");
         String user = resource.getString("user");
         String pass = resource.getString("password");
+        cpds = new ComboPooledDataSource();
         try {
-            Class.forName(driver).newInstance();
-        } catch (ClassNotFoundException e) {
-            throw new DBException("Драйвер не загружен!");
-        } catch (InstantiationException | IllegalAccessException e) {
-            LOG.error(e);
+            cpds.setDriverClass(driver);
+        } catch (PropertyVetoException e) {
+            throw new DBException("Ошибка загрузка драйвера.");
         }
-        return DriverManager.getConnection(url, user, pass);
+        cpds.setJdbcUrl(url);
+        cpds.setUser(user);
+        cpds.setPassword(pass);
+
     }
+
+    public static ConnectionManager getInstance() throws SQLException {
+        if (connectionManager == null) {
+            connectionManager = new ConnectionManager();
+            return connectionManager;
+        } else {
+            return connectionManager;
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.cpds.getConnection();
+    }
+
 }
