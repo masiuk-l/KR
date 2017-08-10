67415de236f78c9df866c5472d58a4bef70eef7f
masiuk-l <masiuk-l@ya.ru>
2017-08-10 10:42:55 +0300

    add jsp sample

---
+++ src/dao/impl/BookDAOImpl.java
@@ -0,0 +1,40 @@
+package dao.impl;
+
+import dao.BookDAO;
+import entity.Book;
+
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+
+public class BookDAOImpl implements BookDAO {
+    @Override
+    public List<Book> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Book> getByName(String name) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Book save(Book book) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Book get(Serializable id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Book book) throws SQLException {
+
+    }
+
+    @Override
+    public int delete(Serializable id) throws SQLException {
+        return 0;
+    }
+}
---
+++ web/readersInfo.jsp
@@ -0,0 +1,27 @@
+<%@ page import="entity.Reader" %>
+<%@ page import="service.ReaderService" %>
+<%@ page import="service.impl.ReaderServiceImpl" %>
+<%@ page import="java.util.List" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Readers</title>
+</head>
+<body>
+<%
+    ReaderService readerService = new ReaderServiceImpl();
+    List<Reader> readers = readerService.getAll();
+    request.setAttribute("readers", readers);
+%>
+
+<table border="1" about="">
+    <c:forEach var="reader" items="${requestScope.get(\"readers\")}">
+        <tr>
+            <td>${reader.name}</td>
+            <td>${reader.surname}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
